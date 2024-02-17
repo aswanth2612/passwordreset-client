935a9862d301e55e5e9d6d5fcb59ee6fe9809841
aswanth2612 <aswanthmmg@gmail.com>
2024-02-17 20:53:07 +0530

    first commit

---
+++ src/Components/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import '../App.css';
+import Axios from 'axios'
+import { useNavigate, Link } from 'react-router-dom'
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigate = useNavigate()
+
+    Axios.defaults.withCrendentials = true;
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        Axios.post('http://localhost:3000/auth/login', {email, password}).then(response =>{
+            if(response.data.status) {
+                navigate('/')
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    return (
+        <div className='sign-up-container'>
+            <form className='sign-up-form' onSubmit={handleSubmit}> 
+                <h2>Login</h2>
+
+                <label htmlFor="email">Email:</label>
+                <input type="email" autoComplete='off' placeholder='Email'
+                    onChange={(e) => setEmail(e.target.value)} />
+
+                <label htmlFor="password">Password:</label>
+                <input type="password" placeholder='********'
+                    onChange={(e) => setPassword(e.target.value)} />
+
+                <button type='submit'>Login</button>
+                <Link to="/forgotPassword">Forgot Password</Link>
+                <p>Don't hava an Account? <Link to="/signup">Sign Up</Link> </p> 
+            </form>
+        </div>
+    )
+}
+
+export default Login
